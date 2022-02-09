8134dc112b7aa26a7114813794b7e3ff4c15d721
Cj-K-7 <kllt7059@gmail.com>
2022-02-09 15:12:10 +0900

    React-Hook-Form

--- src/Schedular.tsx
+++ src/Schedular.tsx
@@ -1,45 +1,79 @@
-import { text } from 'node:stream/consumers';
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
 
-const Container = styled.div``;
-const Form = styled.form``;
-const What = styled.input.attrs({ type : "text", required: true })``;
-const When = styled.input.attrs({ type : "date", required: true })`
-::-webkit-datetime-edit { padding: 0; }
-::-webkit-datetime-edit-fields-wrapper { background: #ffffff; }
-::-webkit-datetime-edit-text { color: red;}
-::-webkit-datetime-edit-month-field { color: blue; }
-::-webkit-datetime-edit-day-field { color: green; }
-::-webkit-datetime-edit-year-field { color: purple; }
-::-webkit-calendar-picker-indicator { background-color: orange; }
+interface IForm {
+  What: string;
+  When: string;
+}
+
+const Container = styled.div`
+`;
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;`;
+const What = styled.input.attrs({ type: "text" })``;
+const When = styled.input.attrs({ type: "datetime-local" })`
+  ::-webkit-datetime-edit {
+    padding: 0;
+  }
+  ::-webkit-datetime-edit-fields-wrapper {
+    background: #ffffff;
+  }
+  ::-webkit-datetime-edit-text {
+    color: red;
+  }
+  ::-webkit-datetime-edit-month-field {
+    color: blue;
+  }
+  ::-webkit-datetime-edit-day-field {
+    color: green;
+  }
+  ::-webkit-datetime-edit-year-field {
+    color: purple;
+  }
+  ::-webkit-calendar-picker-indicator {
+    background-color: orange;
+  }
 `;
 const Btn = styled.button``;
+const Err = styled.span`
+    color: red;
+    font-size: 12px;
+    opacity : 0.5;
+`;
 
 function Schedular() {
-    const [what,setWhat] = useState("");
-    const [when,setWhen] = useState("");
-    const onChange = (event:React.FormEvent<HTMLInputElement>) =>{
-        const {
-          currentTarget: { value },
-        } = event;
-        setWhat(value);
-    }
-    const onSetDate = (event:React.FormEvent<HTMLInputElement>) =>{
-        const {
-          currentTarget: { value },
-        } = event;
-        setWhen(value);
-    }
-    const onSubmit = (event:React.FormEvent<HTMLFormElement>) =>{
-        event.preventDefault();
-        console.log( what, when );
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError
+  } = useForm<IForm>();
+
+  const onValid =(data: any) =>{
+      if(data.What === ""){
+        setError(
+          "What",{},{shouldFocus : true}
+        );
+      }
+  }
+  
   return (
     <Container>
-      <Form onSubmit={onSubmit}>
-        <What placeholder="What" value={what} onChange={onChange} />
-        <When type="date" value={when} onChange={onSetDate} />
+      <Form onSubmit={handleSubmit(onValid)}>
+        <What
+          {...register("What", {
+            required: "Wirte Task / meetings / Events / Anniversaries",
+          })}
+          placeholder="What"
+        />
+        <Err>{errors?.What?.message}</Err>
+        <When
+          {...register("When", {
+            required: "We need date/time",
+          })}
+        />
+        <Err>{errors?.When?.message}</Err>
         <Btn> Add </Btn>
       </Form>
     </Container>
